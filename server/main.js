ad38567d706ac8661be901b346e114de4667f0e1
Eduardo Samrsla <ed.zluhan@gmail.com>
2017-10-02 16:56:52 -0300

    use faker and lodash to mock employees

---
+++ server/main.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import { Meteor } from 'meteor/meteor';
+import { image, helpers } from 'faker';
+import { Employees } from '../imports/collections/employees';
+
+Meteor.startup(() => {
+    const numberOfRecords = Employees.find({}).count();
+    if (!numberOfRecords) {
+
+        _.times(5000, () => {
+            const { name, email, phone } = helpers.createCard();
+
+            Employees.insert({
+                name, email, phone,
+                avatar: image.avatar()
+            });
+        });
+    }
+});
